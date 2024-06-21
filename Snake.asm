2f1f363c05533768f8dda5183334f03d892ace81
Aron Venegas Muñoz <aronvenegas13@gmail.com>
2024-06-21 13:52:11 -0600

    Avance en el movimiento y colisión con bordes
    Se desarrolló el movimiento automático de la serpiente al actualizar las direcciones de la cabeza y la cola, ahora la posición anterior de la serpiente se pinta con el color del fondo y se implementan las funciones y saltos condicionales para la colisión del jugador con los bordes del juego

--- Snake.asm
+++ Snake.asm
@@ -1,22 +1,24 @@
 .data
-	display: .space 1024   # Reserva espacio para el display (256 palabras)
+	display: .space 1024   #Reserva espacio para el display (256 palabras)
+	head: .word 0 #Cabeza de la serpiente
+	tail: .word 0 #Cola de la serpiente
 .text
 	.globl main
 main:
 	li $t1, 0 #Inicializa el contador 
-     li $s3, 0 #Cantidad de frutas
-     jal SetBG #Crea el fondo del juego 
-     jal SetFruit #colocar frutas
- 	jal SetPlayer #Se coloca al jugador
- 	jal PlaySnake #Jugar al juego
-     li $v0, 10       
-     syscall
+	li $s3, 0 #Cantidad de frutas
+	jal SetBG #Crea el fondo del juego 
+	jal SetFruit #colocar frutas
+	jal SetPlayer #Se coloca al jugador
+	jal PlaySnake #Jugar al juego
+	li $v0, 10       
+	syscall
 PlaySnake:
 	move $t9, $ra
-	li $s5, 5 #Direccion inicial (abajo)
+	li $s5, 1 #Direccion inicial (abajo)
 	PlayLoop:
 		li $v0, 32
-		la $a0, 700
+		la $a0, 150 #Delay de 200ms
 		syscall
 		beq $s5, 1, Down
 		beq $s5, 2, Up
@@ -26,39 +28,62 @@ PlaySnake:
 		j PlayLoop
 StopPlaying:
 	jr $t9
+GameOver: #Si se da la condicion, actualiza valores para que acabe el juego
+	li $s5, 5
+	li $s1, 1
+	li $s2, 1
+	j PlayLoop
 
-Down:
+	
+Down: #Mueve la serpiente automaticamente hacia abajo
 	addi $s1, $s1, 1
 	jal CalcPos
+	bgt $s1, 15, GameOver #Verifica si toca un borde
+	sw $t6, head #Actualiza la cabeza
 	jal SetPixel
+	jal UpdateTailAuto
 	j PlayLoop
 
-Up:
+Up: #Mueve la serpiente automaticamente hacia arriba
 	addi $s1, $s1, -1
 	jal CalcPos
+	blt $s1, 0, GameOver #Verifica si toca un borde
+	sw $t6, head #Actualiza la cabeza
 	jal SetPixel
+	jal UpdateTailAuto
 	j PlayLoop
-Right: 
+Right: #Mueve la serpiente automaticamente hacia la derecha
 	addi $s2, $s2, 1
 	jal CalcPos
+	bgt $s2, 15, GameOver #Verifica si toca un borde
+	sw $t6, head #Actualiza la cabeza
 	jal SetPixel
+	jal UpdateTailAuto
 	j PlayLoop
-Left: 
+Left: #Mueve la serpiente automaticamente hacia la izquierda
 	addi $s2, $s2, -1
 	jal CalcPos
+	blt $s2, 0, GameOver #Verifica si toca un borde
+	sw $t6, head #Actualiza la cabeza
 	jal SetPixel
+	jal UpdateTailAuto
 	j PlayLoop
-	
-	
 
+UpdateTailAuto: #Actualizar la cola con el movimiento automatico de la serpiente
+	lw $t8, tail
+	li $t7, 0x008080 #Color de fondo (turquesa)
+	sw $t7, ($t8)
+	sw $t6, tail
+	li $t7, 0x00FF00 #Color de la serpiente (verde lima)
+	jr $ra
 	
-
-SetPlayer: #Colocar al jugador en el juego
+SetPlayer: #Colocar al jugador al inicio del juego
 	move $t9, $ra
 	li $s1, 1
 	li $s2, 7
 	jal CalcPos
-	move $s7, $t6
+	sw $t6, head #Inicializa la posicion de la cabeza
+	sw $t6, tail #Inicializa la posicion de la cola
 	li $t7, 0x00FF00
 	jal SetPixel
 	jr $t9
@@ -91,10 +116,10 @@ CalcPos: #Calcular posicion en el bitmap (Recibe x, y)
 	li $t4, 16        
 	mul $t5, $t4, $s1  
 	add $t5, $t5, $s2  
-     sll $t5, $t5, 2   
-     la $t6, display    
-     add $t6, $t6, $t5  #t6 es la posicion en el bitmap    
-     jr $ra
+	sll $t5, $t5, 2   
+	la $t6, display    
+	add $t6, $t6, $t5  #t6 es la posicion en el bitmap    
+	jr $ra
      
 SetBG: #Crea el background del tablero
 	beq $t1, 1024, EndLoop
